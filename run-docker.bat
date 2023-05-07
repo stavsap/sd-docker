13a43daaa2d2e5df3061537e1a874b14a0dc33bc
Stav Sapir <stavsap@gmail.com>
2023-05-07 19:04:24 +0300

    Update run-docker.bat

--- run-docker.bat
+++ run-docker.bat
@@ -1,5 +1,6 @@
 @echo off
 
+sd-cache
 docker run -it --gpus=all -e COMMANDLINE_ARGS="--listen --xformers" -e LD_LIBRARY_PATH=/usr/lib/x86_64_linux-gnu/ -v G:/sd_docker_volume:/home/user/stable-diffusion-webui -v sd-cache:/home/user/.cache -p 8080:7860 --rm a1111
 
 echo All Complete!!! Press any key to exit...
